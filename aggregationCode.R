1133b0d4f70118b9d371050d65d4bf07da6ba34e
Charlie Bettigole <88738723+bettigole@users.noreply.github.com>
2022-03-15 11:33:07 -0400

    update aggregation code

--- aggregationCode.R
+++ aggregationCode.R
@@ -1,5 +1,5 @@
-library("rstudioapi")  
-setwd(dirname(getActiveDocumentContext()$path)) 
+library("rstudioapi")
+setwd(dirname(getActiveDocumentContext()$path))
 source('code/projectSetup.R')
 source('code/aggregateFun.R')
 source('code/anovaFun.R')
@@ -10,7 +10,7 @@ set.seet<-42
 data<-sb # choose l7, sb, or caney
 nGroups<-20 # max number of aggregates per mgmt unit
 nIter<-10 # number of iterations for models
-sig<-1.96 #1.96 for 95% significance, 1.68 for 90%
+sig<-1.645 #1.96 for 95% significance, 1.645 for 90%
 
 ############### AGGREGATION SECTION !!! ####################
 
